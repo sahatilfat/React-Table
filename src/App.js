cd9b821b684d97c14d2d59671922a495de30d8c0
unknown <sahatrectjs@gmail.com>
2022-03-08 16:02:19 +0700

    sorting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BasicTable from "./components/BasicTable";
+import SortingTable from "./components/SortingTable";
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
+      {/* <BasicTable /> */}
+      <SortingTable />
     </div>
   );
 }
