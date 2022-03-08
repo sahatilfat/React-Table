38d4352caa4739a7e55b0ddce12eae9486f11567
unknown <sahatrectjs@gmail.com>
2022-03-08 18:54:27 +0700

    Global Filtering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import BasicTable from "./components/BasicTable";
 import SortingTable from "./components/SortingTable";
+import FilteringTable from "./components/FilteringTable";
 
 function App() {
   return (
     <div className="App">
       {/* <BasicTable /> */}
-      <SortingTable />
+      {/* <SortingTable /> */}
+      <FilteringTable />
     </div>
   );
 }
